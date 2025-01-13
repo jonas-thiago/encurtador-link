f2bab3bf6ca1e58b0a7f1fde652427a4703a3645
Jonas Thiago <146122741+jonas-thiago@users.noreply.github.com>
2025-01-13 01:34:03 -0300

    version2
    completo

--- shortener/api.py
+++ shortener/api.py
@@ -2,6 +2,9 @@ from ninja import Router
 from .schemas import LinkSchema, UpdateLinkSchema
 from .models import Links, Clicks
 from django.shortcuts import get_list_or_404, redirect
+import qrcode
+from io import BytesIO
+import base64
 
 shortener_router = Router()
 
@@ -57,7 +60,47 @@ def update_link(request, link_id: int, link_schema: UpdateLinkSchema):
     data = link_schema.dict()
 
     token = data['token']
-    if token and Links.objects.filter(token=token).exists():
+    if token and Links.objects.filter(token=token).exclude(id=link_id).exists():
         return 409, {'error': 'Token já existe, use outro'}
 
-    for field, value in data.items()
+    for field, value in data.items():
+        if value:
+            setattr(link, field, value)
+    link.save()
+    return 200, link
+
+
+@shortener_router.get('statistics/{link_id}/', response={200: dict})
+def statistics(request, link_id: int):
+    link = get_list_or_404(Links, id=link_id)
+    uniques_clicks = Clicks.objects.filter(
+        link=link).values('ip').distinct().count()
+    total_clicks = Clicks.objects.filter(link=link).values('ip').count()
+
+    return 200, {'uniques_clicks': uniques_clicks, 'total_clicks': total_clicks}
+
+
+def get_api_url(request, token):
+    scheme = request.scheme
+    host = request.get_host()
+    return f'{scheme}://{host}/api/{token}'
+
+
+@shortener_router.get('qrcode/{link_id}/', response={200: dict})
+def get_qrcode(request, link_id: int):
+    link = get_list_or_404(Links, id=link_id)
+
+    qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=4
+    )
+    qr.add_data(get_api_url(request, link.token))
+    qr.make(fit=True)
+
+    content = BytesIO()
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save(content)
+
+    data = base64.b64encode(content.getvalue()).decode('UTF-8')
+    return 200, {'content_image': data}
